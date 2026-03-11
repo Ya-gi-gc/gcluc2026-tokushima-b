44648278306ab732dc816af210abf0f5f273beca
ebiebia <25010101@anabuki-college.ac.jp>
2026-03-12 01:06:43 +0900

    プレイヤー、敵、タイマー、背景
    プレイヤーのスポーン位置
    敵のスポーン位置
    敵が画面外に出ると敵を削除
    背景の変更（なぜか白黒かつ斜め）
    タイマーの設定

---
+++ 疾風クラッシュ/Sample3/Sample3/Project/GameProject/Field.cpp
@@ -0,0 +1,33 @@
+#include "Field.h"
+
+// コンストラクタ
+Field::Field()
+	: Task((int)ETaskPrio::Field)
+	, mp_fieldImg(nullptr)
+{
+	// フィールド画像を読み込み
+	mp_fieldImg = CImage::CreateImage("haikei.png");
+}
+
+// デストラクタ
+Field::~Field()
+{
+	// フィールドの画像を削除
+	if (mp_fieldImg != nullptr)
+	{
+		delete mp_fieldImg;
+		mp_fieldImg = nullptr;
+	}
+}
+
+// 更新
+void Field::Update()
+{
+}
+
+// 描画
+void Field::PreRender()
+{
+	// フィールドを描画
+	mp_fieldImg->Draw();
+}
---
+++ 疾風クラッシュ/Sample3/Sample3/Project/GameProject/Timer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <time.h>
+
+class Timer
+{
+private:
+	static time_t startTime;
+
+public:
+	static void Start();
+	static void Update();
+};
