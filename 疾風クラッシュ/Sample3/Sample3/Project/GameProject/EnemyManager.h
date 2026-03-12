e55cb9bfaf590c102167c179d82fc4acfec6722b
ebiebia <25010101@anabuki-college.ac.jp>
2026-03-12 15:58:01 +0900

    ゲーム入れ直し

---
+++ 疾風クラッシュ/Sample3/Sample3/Project/GameProject/EnemyManager.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Task.h"
+
+class EnemyBase;
+
+// 敵管理クラス
+class EnemyManager : public Task
+{
+public:
+	// インスタンス取得
+	static EnemyManager* Instance();
+
+	// 敵をリストに追加
+	void Add(EnemyBase* enemy);
+	// 敵をリストから取り除く
+	void Remove(EnemyBase* enemy);
+
+	// 指定した位置から一番近い敵を取得
+	EnemyBase* GetNearEnemy(const CVector3D& pos, const CVector3D& range);
+
+	// 更新
+	void Update() override;
+
+private:
+	// コンストラクタ
+	EnemyManager();
+	// デストラクタ
+	~EnemyManager();
+
+	// インスタンス
+	static EnemyManager* ms_instance;
+	// 生成済みのエネミーのリスト
+	std::vector<EnemyBase*> m_enemies;
+
+	float m_elapsedTime;	// 経過時間計測用
+};
